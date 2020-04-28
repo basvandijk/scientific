cb4e73295eb9dc36e153ec1d0e4e6ccf95283398
Anatolii Prylutskyi <anpryl@gmail.com>
2020-04-28 20:08:32 +0300

    Add Lift instance

--- scientific.nix
+++ scientific.nix
@@ -1,7 +1,7 @@
-{ mkDerivation, base, binary, bytestring, containers, deepseq
-, hashable, integer-gmp, integer-logarithms, QuickCheck, smallcheck, stdenv, tasty
-, tasty-ant-xml, tasty-hunit, tasty-quickcheck, tasty-smallcheck
-, text, primitive, criterion
+{ mkDerivation, base, binary, bytestring, containers, criterion
+, deepseq, hashable, integer-gmp, integer-logarithms, primitive
+, QuickCheck, smallcheck, stdenv, tasty, tasty-ant-xml, tasty-hunit
+, tasty-quickcheck, tasty-smallcheck, template-haskell, text
 }:
 mkDerivation {
   pname = "scientific";
@@ -9,14 +9,13 @@ mkDerivation {
   src = ./.;
   libraryHaskellDepends = [
     base binary bytestring containers deepseq hashable integer-gmp
-    integer-logarithms text primitive
+    integer-logarithms primitive template-haskell text
   ];
   testHaskellDepends = [
-    base bytestring QuickCheck smallcheck tasty tasty-ant-xml
+    base binary bytestring QuickCheck smallcheck tasty tasty-ant-xml
     tasty-hunit tasty-quickcheck tasty-smallcheck text
-
-    criterion
   ];
+  benchmarkHaskellDepends = [ base criterion ];
   homepage = "https://github.com/basvandijk/scientific";
   description = "Numbers represented using scientific notation";
   license = stdenv.lib.licenses.bsd3;
